0b7e83384144d1cbf973da01f454afd40ac6ce28
baekyeeee <baekyeeee@gmail.com>
2026-02-05 19:16:23 +0800

    修正圖片上傳路徑

--- common/cors.php
+++ common/cors.php
@@ -29,4 +29,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   http_response_code(204);
   exit;
 }
-
--- news/news_edit.php
+++ news/news_edit.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
